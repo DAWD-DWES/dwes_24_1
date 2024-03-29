8ec351e5ac21e82a177880c1981bf869ffb79135
Iván <jivanrod@gmail.com>
2024-03-29 16:48:44 +0100

    Initial commit

---
+++ public/assets/js/resolver.js
@@ -0,0 +1,20 @@
+/* Definición del manejador de eventos para el botón de resolver con JQuery
+$(document).ready(function () {
+    $("#botonresolver").click(resolverPartida);
+});
+*/
+
+
+/* Definición del manejador de eventos para el botón de resolver con vanilla JavaScript
+document.addEventListener("DOMContentLoaded", function () {
+    const botonpista = document.getElementById('botonresolver');
+    botonpista.addEventListener("click", resolverPartida);
+});
+*/
+
+
+/* Función para iniciar el proceso AJAX de resolución de partida y recoger los resultados y mostrarlos en la página
+
+function resolverPartida(e) {}
+;
+*/
