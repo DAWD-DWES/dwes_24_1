13274d951eb4e0112e3d56116b2363a78aac1b22
Iván <jivanrod@gmail.com>
2024-04-01 19:48:21 +0200

    Update resolver.js

--- public/assets/js/resolver.js
+++ public/assets/js/resolver.js
@@ -1,27 +1,54 @@
-/* Definición del manejador de eventos para el botón de resolver con JQuery
+// Descomenta esta sección si vas a programar con JQuery
+
+// Inicio Sección JQuery
+
+// Definición del manejador de eventos para el botón de resolver (JQuery) 
 $(document).ready(function () {
     $("#botonresolverpartida").click(resolverPartida);
 });
-*/
+// Función para iniciar el proceso AJAX de resolución de partida y recoger los resultados y mostrarlos en la página (JQuery) 
+function resolverPartida(e) {
+
+}
+;
+// Función que muestra un texto en el elemento cuyo identificador es id (Jquery)
+function muestraTexto(id, texto) {
+    $("#${id}").text(texto);
+}
 
 
-/* Definición del manejador de eventos para el botón de resolver con vanilla JavaScript
+// Función que deshabilita un botón dado su id (Jquery)
+function deshabilitaBoton(idBoton) {
+    $("#${idBoton}").prop('disabled', true);
+}
+
+// Fin sección JQuery
+
+// Descomenta esta sección si vas a programar con JavaScript puro
+
+// Inicio Sección JavaScript puro
+
+// Definición del manejador de eventos para el botón de resolver (JavaScript puro) 
 document.addEventListener("DOMContentLoaded", function () {
     const botonresolverpartida = document.getElementById('botonresolverpartida');
     botonresolverpartida.addEventListener("click", resolverPartida);
 });
-*/
 
+// Función para iniciar el proceso AJAX de resolución de partida y recoger los resultados y mostrarlos en la página (JavaScript puro) 
+function resolverPartida(e) {
 
-/* Función para iniciar el proceso AJAX de resolución de partida y recoger los resultados y mostrarlos en la página
-
-function resolverPartida(e) {};
-*/
-
-function muestraMensaje(mensaje) {
-    $("#mensaje").text(mensaje);
 }
 
-function muestraPalabra(palabra) {
-    $("#palabra").text(palabra.split('').join(' '));
+// Función que muestra el mensaje después de comprobar la palabra usada para resolver (JavaScript puro)
+function muestraTexto(id, texto) {
+    const element = document.getElementById(id);
+    element.innerText = texto;
 }
+
+
+// Función que deshabilita un botón dado su id (Javascript puro)
+function deshabilitaBoton(idBoton) {
+    document.getElementById(idBoton).disabled = true;
+}
+
+// Fin sección JavaScript puro
